a622ebe4bb1f789a94e94648f93cbcb08669a752
tadaspetrikas <67460225+tadaspetrikas@users.noreply.github.com>
2022-11-03 21:37:18 +0200

    Init

---
+++ CodeAcademyChat/CodeAcademyChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeAcademyChat
+//
+//  Created by Tadas Petrikas on 2022-11-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

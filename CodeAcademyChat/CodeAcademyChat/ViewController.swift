519be5d7883b462f06d68f402fe01442ce5fc298
rpromas <rpromas@gmail.com>
2022-11-07 19:05:13 +0200

    recent changes

--- CodeAcademyChat/CodeAcademyChat/ViewController.swift
+++ CodeAcademyChat/CodeAcademyChat/ViewController.swift
@@ -2,18 +2,46 @@
 //  ViewController.swift
 //  CodeAcademyChat
 //
-//  Created by Tadas Petrikas on 2022-11-03.
+//  Created by Romas Petkus on 2022-11-07.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    enum State {
+        case register
+        case login
+    }
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    var currentState: State = .register
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onSegmentChange(_ sender: Any) {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            currentState = .register
+        } else if segmentedControl.selectedSegmentIndex == 1 {
+            currentState = .login
+        }
+        
+        confirmPasswordTextField.isHidden = currentState != .register
+        
+        switch currentState {
+        case .register:
+            actionButton.titleLabel?.text = "Register"
+        case .login:
+            actionButton.titleLabel?.text = "Login"
+        }
+    }
+    
 }
 

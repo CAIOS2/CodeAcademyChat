925c93e89356adce96af70d0514e222ba9295f7a
DeividasZabulis <114018237+DeividasZabulis@users.noreply.github.com>
2022-11-07 20:57:25 +0200

    changes

---
+++ CodeAcademyChat/CodeAcademyChat/Data/UserManager.swift
@@ -0,0 +1,32 @@
+//
+//  UserManager.swift
+//  CodeAcademyChat
+//
+//  Created by Deividas Zabulis on 2022-11-07.
+//
+
+import Foundation
+
+
+class UserManager {
+    
+    var userList: [User] = []
+    
+    func register(username: String, password: String, confirmPassword: String) {
+        guard !username.isEmpty,!password.isEmpty
+        else {
+            return
+        }
+        if password != confirmPassword {
+            return
+        }
+        for user in userList {
+            if username == user.username {
+                return
+            }
+        }
+        
+        let user = User(username: username, password: password, isOnline: true)
+        userList.append(user)
+    }
+}
--- CodeAcademyChat/CodeAcademyChat/ViewController.swift
+++ CodeAcademyChat/CodeAcademyChat/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let userManager: UserManager = UserManager()
+    enum State {
+        case register
+        case login
+    }
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    @IBOutlet weak var actionButton: UIButton!
+    
+    var currentState: State = .login
+    
+    //    var users: [User]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonClick(_ sender: Any) {
+        
+        userManager.register(username: usernameTextField.text!, password: passwordTextField.text!, confirmPassword: confirmPasswordTextField.text!)
+    }
+    @IBAction func onSegmentChange(_ sender: Any) {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            currentState = .register
+        } else if segmentedControl.selectedSegmentIndex == 1 {
+            currentState = .login
+        }
+        
+        confirmPasswordTextField.isHidden = currentState != .register
+        //        confirmPasswordTextField.isHidden = segmentedControl.selectedSegmentIndex != 0
+        switch currentState {
+        case .register:
+            actionButton.titleLabel?.text = "Register"
+        case .login:
+            actionButton.titleLabel?.text = "Login"
+        }
+        
+    }
 }
-

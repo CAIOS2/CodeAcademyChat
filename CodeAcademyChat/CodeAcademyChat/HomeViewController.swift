da24297cae92d17038205f6827f4445e28ddf70b
pangolio <104528863+pangolio@users.noreply.github.com>
2022-11-14 17:53:49 +0200

    modified:   CodeAcademyChat/CodeAcademyChat/Base.lproj/Main.storyboard 	modified:   CodeAcademyChat/CodeAcademyChat/HomeViewController.swift 	modified:   CodeAcademyChat/CodeAcademyChat/SettingsViewController.swift

--- CodeAcademyChat/CodeAcademyChat/HomeViewController.swift
+++ CodeAcademyChat/CodeAcademyChat/HomeViewController.swift
@@ -36,7 +36,26 @@ class HomeViewController: UIViewController {
         self.present(alertController, animated: true)
     }
     
+    @IBAction func createNewRoomTapped(_ sender: Any) {
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let alertController = UIAlertController(title: "Error creating room", message: "Room name can't be empty!", preferredStyle: .alert)
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true)
+    }
     
+    @IBAction func showOnlineUserTapped(_ sender: Any) {
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let alertController = UIAlertController(title: "Online users", message: "\(user.username)", preferredStyle: .alert)
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true)
+    }
+    
+    @IBAction func showOflineUserTapped(_ sender: Any) {
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let alertController = UIAlertController(title: "Ofline users", message: "", preferredStyle: .alert)
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true)
+    }
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
--- CodeAcademyChat/CodeAcademyChat/SettingsViewController.swift
+++ CodeAcademyChat/CodeAcademyChat/SettingsViewController.swift
@@ -20,5 +20,32 @@ class SettingsViewController: UIViewController {
         usernameLabel.text = user.username
     }
     
+    @IBAction func userEditTapped(_ sender: Any) {
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        let alertController = UIAlertController(title: "Edit Username", message: "Enter your new username", preferredStyle: .alert)
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        alertController.addTextField { textField in textField.placeholder = "Username"
+        }
+        
+        self.present(alertController, animated: true)
+    }
+    
+    @IBAction func passwordEditTapped(_ sender: Any) {
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        let alertController = UIAlertController(title: "Edit Password", message: "Enter your new password", preferredStyle: .alert)
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        alertController.addTextField { textField in textField.placeholder = "Password"
+        }
+        
+        self.present(alertController, animated: true)
+    }
 }
 

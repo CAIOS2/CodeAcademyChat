1a08edebff56c0b871f793a666e57d0c6cb7cdc4
rpromas <rpromas@gmail.com>
2022-11-15 20:51:23 +0200

    hidde not

--- CodeAcademyChat/CodeAcademyChat/Modules/Room/RoomViewController.swift
+++ CodeAcademyChat/CodeAcademyChat/Modules/Room/RoomViewController.swift
@@ -24,6 +24,7 @@ class RoomViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
         //welcomeLabel.text = "Welcome to \(room.name)"
     }
 
